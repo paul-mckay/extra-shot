5dca903d79a128ca333c83d7e3fb4232fd9b21e4
Paul McKay <pmckay@outlook.com>
2015-11-27 07:49:32 +1300

    Added setup.m

--- matlab/caffe_plot.m
+++ matlab/caffe_plot.m
@@ -1,7 +1,7 @@
 % Copyright (c) 2015 Paul McKay
 % All rights reserved
 %  
-% This file is part of 'caffe-extra-shots', a collection of utilities for 
+% This file is part of 'caffe-extra-shot', a collection of utilities for 
 % Caffe and is subject to the terms of the BSD license.  If a copy of the
 % license was not distributed with this file, you can get one at: 
 % https://raw.githubusercontent.com/pmckay/caffe-extra-shots/master/LICENSE
